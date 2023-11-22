b55ddb35b4337720d54a45b4a7c7b181029f7cb9
kumar-QA <prasannakumar.m.qa@gmail.com>
2023-11-22 17:15:58 +0530

    array single dimenisonal array

---
+++ src/arrays/SingleDimensionalArray.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+		
+		String names[]=new String[5];
+		   names[0]="kumar";
+		   names[1]="sai";
+		   names[2]="prasanna";
+		   names[3]="teja";
+		   names[4]="raja";
+		   
+		   System.out.println(names.length);
+		   
+		  for (int i = 0; i <= names.length; i++) {
+			System.out.println(names[i]);
+		} 
+		   
+		   
+		
+	}
+
+}
